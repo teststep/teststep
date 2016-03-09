9434193988410ca37862b5a382bcac98683e1b4e
Valeriy Shagur <vshagur@gmail.com>
2016-03-09 11:18:02 +0700

    next commit

--- etc/hello.py
+++ etc/hello.py
@@ -1,9 +1,14 @@
-bind="0.0.0.0:8080"
+#bind="0.0.0.0:8080"
 
-def app (environ, start_response):
+#def app (environ, start_response):
+#    status = '200 OK'
+#    response_headers = [('Content-type','text/plain')]
+#    start_response(status, response_headers)
+#    options = environ['QUERY_STRING'].split("&")
+#    return [i+'\n' for i in options]
+def app (environ,start_response):
     status = '200 OK'
-    response_headers = [('Content-type','text/plain')]
-    start_response(status, response_headers)
-    options = environ['QUERY_STRING'].split("&")
-    return [i+'\n' for i in options]
-
+    headers= [('Content-type','text/plain')]
+    start_response(status,headers)
+    body="\r\n".join(environ['QUERY_STRING'].split('&'))
+    return [body]
--- etc/init.sh
+++ etc/init.sh
@@ -3,4 +3,6 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+cd /home/box/web/
+sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:app
 
