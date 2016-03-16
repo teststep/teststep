f673266309d0b854e96d6dc9e64e5194de37d051
Viacheslav Shaposhnikov <shaposhnikov89@gmail.com>
2016-03-17 00:58:58 +0600

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-bind="0.0.0.0:8080"
-
-def app (environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type','text/plain')]
-    start_response(status, response_headers)
-    options = '\r\n'.join(environ['QUERY_STRING'].split("&"))
-    return [options]
-
